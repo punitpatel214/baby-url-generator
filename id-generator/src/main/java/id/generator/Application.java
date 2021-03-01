4e2a98a0e8d4272986b2c6c9d81fc8c25448c484
punitpatel <ppatel@equalexperts.com>
2021-03-01 22:33:51 +0530

    initial commit

---
+++ id-generator/src/main/java/id/generator/Application.java
@@ -0,0 +1,10 @@
+package id.generator;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
