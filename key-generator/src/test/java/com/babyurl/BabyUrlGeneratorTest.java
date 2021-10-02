e64e818006f8110ae45648ee288de2d9a8f0516e
punitpatel <ppatel@equalexperts.com>
2021-10-02 15:05:39 +0530

    Baby-0: add file structure for key generator

---
+++ key-generator/src/test/java/com/babyurl/BabyUrlGeneratorTest.java
@@ -0,0 +1,21 @@
+package com.babyurl;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class BabyUrlGeneratorTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
