b4929bca29023725eef7776d40542942143ab11e
punitpatel <ppatel@equalexperts.com>
2021-10-04 19:19:48 +0530

    Baby-1: check Github CI action

--- key-generator/src/test/java/com/babyurl/keygeneator/AppTest.java
+++ key-generator/src/test/java/com/babyurl/keygeneator/AppTest.java
@@ -6,6 +6,9 @@ import jakarta.inject.Inject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
+
 @MicronautTest
 class KeyGeneratorAPITest {
 
@@ -15,6 +18,9 @@ class KeyGeneratorAPITest {
     @Test
     void testItWorks() {
         Assertions.assertTrue(application.isRunning());
+
+        // verify CI
+        fail("Check GitHub CI Integration");
     }
 
 }
