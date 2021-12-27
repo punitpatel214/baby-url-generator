623c579ffbc57df8013fb6448bf5bca98b9c70a3
punitpatel <ppatel@equalexperts.com>
2021-12-27 17:33:19 +0530

    baby-4: correct

--- key-generator/src/test/java/com/babyurl/keygeneator/KeyGeneratorApplicationTest.java
+++ key-generator/src/test/java/com/babyurl/keygeneator/KeyGeneratorApplicationTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 @MicronautTest
-class KeyGeneratorKeyGeneratorApplicationTest {
+public class KeyGeneratorApplicationTest {
 
     @Inject
     EmbeddedApplication<?> application;
