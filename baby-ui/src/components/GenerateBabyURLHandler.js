5d3b38b11e9cf3daa5f7626eeb0fba8ddcd5b4c3
punit-ee <patelp2@science.regn.net>
2021-12-31 13:37:28 +0530

    baby-19: add env file for api

--- baby-ui/src/components/GenerateBabyURLHandler.js
+++ baby-ui/src/components/GenerateBabyURLHandler.js
@@ -1,10 +1,12 @@
+import { API } from "../../settings/constants"
+
 export default function generateBabyURL(longUrl, setBabyUrlData) {
   const requestOptions = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ url: longUrl }),
   }
-  fetch(`http://localhost:8080/shortenURL`, requestOptions)
+  fetch(API.shortenURL, requestOptions)
     .then((res) => res.json())
     .then((res) => setBabyUrlData({ longUrl: longUrl, babyUrl: res.babyURL }))
 }
---
+++ baby-ui/src/settings/constants.js
@@ -0,0 +1,7 @@
+export const BASE_URL = {
+  BABY_URL: process.env.BABY_URL || "",
+}
+
+export const API = {
+  SHORTEN_URL: `${BASE_URL.BABY_URL}/shortenURL`,
+}
